447374f3136c0a9ef043f50a1c584e90a1b8b1e5
Michał <m.majewski.dm@gmail.com>
2025-08-18 08:27:25 +0200

    feat: add second slider in promotions section

--- js/main.js
+++ js/main.js
@@ -48,7 +48,6 @@
       }, 300);
   }
 
-  // Event Listeners
   $menuTrigger.on('click', function(e) {
       e.preventDefault();
       openMenu();
@@ -89,7 +88,7 @@
     `);
   });
 
-// Slick slider 
+// Slick slider Hero Section
 
   $('.slider__wrapper').slick({
         dots: true,
@@ -131,4 +130,121 @@
       $('.cart-preview, .cart-overlay').hide();
     }, 300);
   });
+
+
+  // Promotions Slider Section
+
+  //  Mock data
+  const productsData = {
+    promotions: [
+      { name: 'Półbuty mokasyny na grubej podeszwie', price: '350,10zł', img: 'assets/product-1-slider.png', labels: [{type: 'promo', text: 'PROMOCJA'}, {type: 'new', text: 'NOWOŚĆ'}] },
+      { name: 'Eleganckie czółenka skórzane', price: '299,99zł', img: 'assets/product-2-slider.png', labels: [{type: 'promo', text: 'PROMOCJA'}] },
+      { name: 'Wygodne sandały na lato', price: '180,50zł', img: 'assets/product-1-slider.png', labels: [{type: 'promo', text: 'PROMOCJA'}, {type: 'bestseller', text: 'BESTSELLER'}] },
+      { name: 'Stylowe botki jesienne', price: '420,00zł', img: 'assets/product-2-slider.png', labels: [{type: 'promo', text: 'PROMOCJA'}] },
+      { name: 'Klasyczne baleriny damskie', price: '150,00zł', img: 'assets/product-1-slider.png', labels: [{type: 'promo', text: 'PROMOCJA'}] },
+      { name: 'Sportowe sneakersy miejskie', price: '310,00zł', img: 'assets/product-2-slider.png', labels: [{type: 'promo', text: 'PROMOCJA'}] },
+      { name: 'Kozaki za kolano zamszowe', price: '550,00zł', img: 'assets/product-1-slider.png', labels: [] },
+      { name: 'Letnie klapki na platformie', price: '210,00zł', img: 'assets/product-2-slider.png', labels: [{type: 'promo', text: 'PROMOCJA'}] },
+    ],
+    news: [
+      { name: 'Nowoczesne jazzówki z Włoch', price: '480,00zł', img: 'assets/product-1-slider.png', labels: [{type: 'new', text: 'NOWOŚĆ'}] },
+      { name: 'Awangardowe półbuty na koturnie', price: '390,00zł', img: 'assets/product-2-slider.png', labels: [{type: 'new', text: 'NOWOŚĆ'}] },
+      { name: 'Minimalistyczne botki białe', price: '410,00zł', img: 'assets/product-1-slider.png', labels: [{type: 'new', text: 'NOWOŚĆ'}] },
+      { name: 'Skórzane kozaki w stylu retro', price: '620,00zł', img: 'assets/product-2-slider.png', labels: [{type: 'new', text: 'NOWOŚĆ'}] },
+    ],
+    bestsellers: [
+      { name: 'Bestsellerowe mokasyny', price: '350,10zł', img: 'assets/product-1-slider.png', labels: [{type: 'bestseller', text: 'BESTSELLER'}] },
+      { name: 'Bestsellerowe sneakersy', price: '310,00zł', img: 'assets/product-2-slider.png', labels: [{type: 'bestseller', text: 'BESTSELLER'}] },
+      { name: 'Bestsellerowe botki', price: '420,00zł', img: 'assets/product-1-slider.png', labels: [{type: 'bestseller', text: 'BESTSELLER'}, {type: 'promo', text: 'PROMOCJA'}] },
+      { name: 'Bestsellerowe sandały', price: '180,50zł', img: 'assets/product-2-slider.png', labels: [{type: 'bestseller', text: 'BESTSELLER'}] },
+      { name: 'Bestsellerowe czółenka', price: '299,99zł', img: 'assets/product-1-slider.png', labels: [{type: 'bestseller', text: 'BESTSELLER'}] },
+    ]
+  };
+
+  
+  function renderProductCards(products) {
+    let html = '';
+    products.forEach(product => {
+      let labelsHtml = product.labels.map(label => 
+        `<span class="product-card__label product-card__label--${label.type}">${label.text}</span>`
+      ).join('');
+
+      html += `
+        <div>
+          <div class="product-card">
+            <div class="product-card__labels">${labelsHtml}</div>
+            <img src="${product.img}" alt="${product.name}" class="img-fluid" loading="lazy" />
+            <div class="product-card__body">
+                <h3 class="product-card__name">${product.name}</h3>
+                <div class="product-card__price">${product.price}</div>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+    return html;
+  }
+
+  const productSliderSettings = {
+    dots: true,
+    arrows: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    appendArrows: $('.promotions__slider-wrapper'),
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        }
+      }
+    ]
+  };
+
+  function loadProducts(filter) {
+    const $slider = $('.promotions__slider');
+    
+    if ($slider.hasClass('slick-initialized')) {
+      $slider.slick('unslick');
+    }
+    
+    const productsHtml = renderProductCards(productsData[filter]);
+    $slider.html(productsHtml);
+    
+    $slider.slick(productSliderSettings);
+  }
+
+  $('.promotions__tab').on('click', function(e) {
+    e.preventDefault();
+    const $this = $(this);
+    const filter = $this.data('filter');
+
+    $('.promotions__tab').removeClass('active');
+    $this.addClass('active');
+
+    loadProducts(filter);
+  });
+
+  loadProducts('promotions');
+
   });
